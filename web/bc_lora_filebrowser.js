e34b7aa9c982b8ada02e08bf5e9dae60f5b8f64b
78 Ventures <163181482+78Ventures@users.noreply.github.com>
2025-09-09 21:20:21 -0700

    Another attempt at file browsing

--- web/bc_lora_filebrowser.js
+++ web/bc_lora_filebrowser.js
@@ -6,36 +6,47 @@
 import { app } from "../../scripts/app.js";
 
 console.log("🐻 Bear Cave: Loading FileBrowserAPI integration...");
+alert("🐻 Bear Cave: Extension Loading!");
 
 app.registerExtension({
     name: "BearCave.BC_LORA_DEFINE.FileBrowser",
     
     async beforeRegisterNodeDef(nodeType, nodeData) {
         if (nodeData.name === "BC_LORA_DEFINE") {
-            console.log("🐻 Bear Cave: Adding FileBrowserAPI to BC_LORA_DEFINE");
+            console.log("🐻 Bear Cave: Processing BC_LORA_DEFINE node");
             
             const onNodeCreated = nodeType.prototype.onNodeCreated;
             nodeType.prototype.onNodeCreated = function() {
                 const result = onNodeCreated?.apply(this, arguments);
                 
-                // Find the project_base_path widget
-                const projectPathWidget = this.widgets.find(w => w.name === "project_base_path");
-                if (projectPathWidget) {
-                    console.log("🐻 Bear Cave: Found project_base_path widget, adding browse button");
-                    
-                    // Add a browse button using FileBrowserAPI
-                    this.addWidget("button", "📁 Browse Project Folder", null, () => {
-                        if (app.FileFolderAPI) {
-                            console.log("🐻 Bear Cave: Opening folder browser via FileBrowserAPI");
-                            app.FileFolderAPI.open(projectPathWidget, 'folder');
-                        } else {
-                            console.error("🐻 Bear Cave: FileBrowserAPI not available");
-                            alert("FileBrowserAPI not found. Please install ComfyUI-FileBrowserAPI extension.");
-                        }
-                    });
-                } else {
-                    console.error("🐻 Bear Cave: project_base_path widget not found");
-                }
+                console.log("🐻 Bear Cave: BC_LORA_DEFINE node created");
+                console.log("🐻 Bear Cave: Available widgets:", this.widgets?.map(w => w.name));
+                console.log("🐻 Bear Cave: FileFolderAPI available:", !!app.FileFolderAPI);
+                
+                // Wait a bit for all widgets to be created
+                setTimeout(() => {
+                    const projectPathWidget = this.widgets.find(w => w.name === "project_base_path");
+                    if (projectPathWidget) {
+                        console.log("🐻 Bear Cave: Found project_base_path widget, adding browse button");
+                        
+                        // Add a browse button using FileBrowserAPI
+                        this.addWidget("button", "📁 Set Project Path", null, () => {
+                            console.log("🐻 Bear Cave: Browse button clicked");
+                            if (app.FileFolderAPI) {
+                                console.log("🐻 Bear Cave: Opening folder browser via FileBrowserAPI");
+                                app.FileFolderAPI.open(projectPathWidget, 'folder');
+                            } else {
+                                console.error("🐻 Bear Cave: FileBrowserAPI not available");
+                                alert("FileBrowserAPI not found. Please make sure ComfyUI-FileBrowserAPI extension is installed and loaded.");
+                            }
+                        });
+                        
+                        console.log("🐻 Bear Cave: Browse button added successfully");
+                    } else {
+                        console.error("🐻 Bear Cave: project_base_path widget not found");
+                        console.error("🐻 Bear Cave: Available widgets:", this.widgets?.map(w => w.name));
+                    }
+                }, 100);
                 
                 return result;
             };
@@ -48,13 +59,13 @@ app.registerExtension({
                 const projectPathWidget = this.widgets.find(w => w.name === "project_base_path");
                 if (projectPathWidget) {
                     menuOptions.unshift({
-                        content: "🐻 Browse Project Folder",
+                        content: "🐻 Set Project Path",
                         callback: () => {
                             if (app.FileFolderAPI) {
                                 console.log("🐻 Bear Cave: Opening folder browser via right-click menu");
                                 app.FileFolderAPI.open(projectPathWidget, 'folder');
                             } else {
-                                alert("FileBrowserAPI not found. Please install ComfyUI-FileBrowserAPI extension.");
+                                alert("FileBrowserAPI not found. Please make sure ComfyUI-FileBrowserAPI extension is installed.");
                             }
                         }
                     }, null);
